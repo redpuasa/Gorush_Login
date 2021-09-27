6a56fefcc5aabad873049cb857fdbdfc40dd4d8a
Farid Puasa <redpuasa@gmail.com>
2021-09-27 17:44:24 +0800

    update key to 2.0

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
 module.exports = {
-    MongoURI: `mongodb://grpharmacy:gsb2332065@cluster0-shard-00-00.gqcg4.mongodb.net:27017,
-    cluster0-shard-00-01.gqcg4.mongodb.net:27017,cluster0-shard-00-02.gqcg4.mongodb.net:27017/
-    gorushwebsite?ssl=true&replicaSet=atlas-s2qf5u-shard-0&authSource=admin&retryWrites=true&w=majority`
+    MongoURI: `mongodb://grpharmacy:gsb2332065@cluster0-shard-00-00.gqcg4.mongodb.net:27017,cluster0-shard-00-01.gqcg4.mongodb.net:27017,cluster0-shard-00-02.gqcg4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-s2qf5u-shard-0&authSource=admin&retryWrites=true&w=majority`
 }
