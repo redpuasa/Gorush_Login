4d67177eb7fea19725e4e39dd51d4402b7dfb3b7
Farid Puasa <redpuasa@gmail.com>
2021-10-05 14:19:54 +0800

    24th commit

--- routes/app.js
+++ routes/app.js
@@ -228,7 +228,24 @@ router.get('/edit', (req,res) => {
 router.post('/editconfirm', (req,res) => {
     console.log(req.body.name)
     console.log(currentUser._id)
-    User.findByIdAndUpdate({_id: currentUser._id}, {name: req.body.name}, (err,docs) => {
+    User.findByIdAndUpdate({_id: currentUser._id}, {
+        name: req.body.name,
+        icNumber: req.body.icNumber,
+        dob: req.body.dob,
+        password: req.body.password,
+        kampong: req.body.address_1,
+        jalan: req.body.address_2,
+        simpang: req.body.address_3,
+        house_Number: req.body.address_4,
+        contact_1: req.body.contact_1,
+        contact_2: req.body.contact_2,
+        bruhims: req.body.bruhims,
+        pay_MOH: req.body.radioMOH,
+        jpmc: req.body.jpmc,
+        pay_JPMC: req.body.radioJPMC,
+        panaga: req.body.panaga,
+        pay_PHC: req.body.radioPHC,
+    }, (err,docs) => {
         if(err){
             console.log(err)
             res.render('error')
